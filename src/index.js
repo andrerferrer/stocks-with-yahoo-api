3e29020625ea88921ff40379dc7d8b49c56fd416
andre ferrer <andrerferrer@hotmail.com>
2021-12-18 09:32:09 -0300

    update livecode

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const updateTheDomWithApiData = (marketPrice, companyName, currency) => {
   // why no #results here?
   const resultsDiv = document.getElementById('results');
 
-  resultsDiv.innerHTML = `${companyName} current price is ${currency} ${marketPrice}`;
-}
+  resultsDiv.innerHTML = `
+    ${companyName} current price is ${currency} ${marketPrice}
+  `;
+};
 
 const callYahooAPI = (stockSymbol) => {
   // "prepare the API" that we will call
@@ -22,7 +24,7 @@ const callYahooAPI = (stockSymbol) => {
     .then((data) => {
       // understand what data is
       console.log(data);
-      // extract info from the data
+      // extract info from the data (dig the json)
       const marketPrice = data.quoteResponse.result[0].regularMarketPrice;
       const companyName = data.quoteResponse.result[0].shortName;
       const currency = data.quoteResponse.result[0].currency;
@@ -33,18 +35,20 @@ const callYahooAPI = (stockSymbol) => {
 };
 
 const findStockOnline = (event) => {
+  // why do we do this?
   event.preventDefault();
+
   // Who is form?
   console.log(form);
   // Who is event.currentTarget?
   console.log(event.currentTarget);
+  // are they equal
+  console.log(form === event.currentTarget);
 
   const input = event.currentTarget.querySelector('input');
   // what's the diff between input and input.value?
   const stockSymbol = input.value;
   callYahooAPI(stockSymbol);
 };
-
-
 
 form.addEventListener('submit', findStockOnline);
