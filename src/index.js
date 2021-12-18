6f64baa03c912f37742cb417fc0980b0fc092aed
andre ferrer <andrerferrer@hotmail.com>
2021-12-18 08:50:48 -0300

    finish livecode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log("Hello from src/index.js!");
+const form = document.querySelector('form');
+
+const updateTheDomWithApiData = (marketPrice, companyName, currency) => {
+  // why no #results here?
+  const resultsDiv = document.getElementById('results');
+
+  resultsDiv.innerHTML = `${companyName} current price is ${currency} ${marketPrice}`;
+}
+
+const callYahooAPI = (stockSymbol) => {
+  // "prepare the API" that we will call
+  const url = `https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=${stockSymbol}`;
+  const options = {
+    headers: {
+      'x-api-key': 'De5INgjxEKalH4yBPSg4r63mVPNSLAHs7y5qmAmm'
+    }
+  };
+
+  // call the API
+  fetch(url, options)
+    .then(res => res.json())
+    .then((data) => {
+      // understand what data is
+      console.log(data);
+      // extract info from the data
+      const marketPrice = data.quoteResponse.result[0].regularMarketPrice;
+      const companyName = data.quoteResponse.result[0].shortName;
+      const currency = data.quoteResponse.result[0].currency;
+
+      // do something with that data
+      updateTheDomWithApiData(marketPrice, companyName, currency);
+    });
+};
+
+const findStockOnline = (event) => {
+  event.preventDefault();
+  // Who is form?
+  console.log(form);
+  // Who is event.currentTarget?
+  console.log(event.currentTarget);
+
+  const input = event.currentTarget.querySelector('input');
+  // what's the diff between input and input.value?
+  const stockSymbol = input.value;
+  callYahooAPI(stockSymbol);
+};
+
+
+
+form.addEventListener('submit', findStockOnline);
